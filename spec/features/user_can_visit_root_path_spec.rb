60ea85449495a2c90c50162ea82de1e2432e7d2b
Jane Greene <janegreenecasa@gmail.com>
2020-09-11 09:23:59 -0600

    added first feature test

--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -8,4 +8,26 @@ describe "User can visit root path" do
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
   end
+  it "to search by author name" do
+    visit root_path
+    fill_in "author", with: 'Emily'
+    click_on "Get Poems"
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_css('.poem', count: 10)
+    within(first('.poem')) do
+      expect(page).to have_css('.title')
+      title = find('.title').text
+      expect(title).not_to be_empty
+      expect(page).to have_css('.author')
+      author = find('.author').text
+      expect(author).not_to be_empty
+      expect(page).to have_css('.content')
+      content = find('.content').text
+      expect(content).not_to be_empty
+      expect(page).to have_css('.tone')
+      tone = find('.tone').text
+      expect(tone).not_to be_empty
+    end
+  end
 end
